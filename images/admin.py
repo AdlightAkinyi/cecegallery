4ebc36c28d419a8cad76d6e310854f6db5a1601e
C0159412020 <adlightakinyi1@gmail.com>
2022-05-30 17:33:27 +0300

    added admin site

--- images/admin.py
+++ images/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from django.contrib import admin
+from .models import Category,Location,Image
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    filter_horizontal =('image',)
+
+admin.site.register(Category)
+admin.site.register(Location)
+admin.site.register(Image)
